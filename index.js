3cd826dbb64afbd35d4ee33d5ddda3112c51320d
Sam Zofkie <samzofkie@gmail.com>
2024-02-06 11:56:02 -0600

    Removed <img> tag from index.html, programatically create it in index.js.

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ document.body.style.backgroundColor = '#b0b0ab';
 document.body.style.fontFamily = 'Arial, Helvetica, sans-serif';
 document.body.style.boxSizing = 'border-box';
 
+let logoImage = document.createElement('img');
+logoImage.src = 'jsn-logo-transparent.png';
+logoImage.alt = 'Jail Solidarity Network logo';
+logoImage.width = '500';
+document.body.appendChild(logoImage);
+
 let deck = new TestimonyDeck();
