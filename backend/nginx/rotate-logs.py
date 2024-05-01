7df7010313e91966a1c19a77f9b0134d29cc6b4d
Sam Zofkie <samzofkie@gmail.com>
2024-05-01 16:16:41 -0500

    Wrote out rotate-logs.py to rotate logs the way I want.

--- backend/nginx/rotate-logs.py
+++ backend/nginx/rotate-logs.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+import shutil
+import subprocess
 from datetime import datetime
 
-with open('/' + str(datetime.now().minute), 'w') as f:
-  pass
+date = datetime.now()
+shutil.copy('/var/log/nginx/access.log', f"/logs/access.log.{date.month}.{date.day - 1}.{date.year}")
+shutil.copy('/var/log/nginx/error.log', f"/logs/error.log.{date.month}.{date.day - 1}.{date.year}")
+
+subprocess.run(['truncate', '--size', '0', '/var/log/nginx/access.log'])
+subprocess.run(['truncate', '--size', '0', '/var/log/nginx/error.log'])
