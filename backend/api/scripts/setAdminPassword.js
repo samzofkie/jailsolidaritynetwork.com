52f645ebed5484572858108286726c99065c5759
Sam Zofkie <samzofkie@gmail.com>
2024-08-19 10:49:47 -0500

    Fixed setAdminPassword script.

--- backend/api/scripts/setAdminPassword.js
+++ backend/api/scripts/setAdminPassword.js
@@ -1,14 +1,22 @@
 #!/usr/bin/node
 const crypto = require('crypto');
-const { DatabaseConnection } = require('./DatabaseConnection');
+const { Client } = require('pg');
+
 
 (async () => {
-  const connection = new DatabaseConnection;
+  const client = new Client({
+    user: 'postgres',
+    host: 'db',
+    database: 'jailsolidaritynetwork',
+    password: 'xGfKqmOznGVrzHc40WY-Y',
+  });
+  await client.connect();
   const password = process.argv.pop();
   const salt = crypto.randomBytes(20);
   const hash = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha3-512');
-  await connection.query(
+  await client.query(
     `INSERT INTO users (name, salt, hash) VALUES ($1, $2, $3)`,
     ['admin', salt.toString('hex'), hash.toString('hex')]
   );
+  await client.end();
 })()
