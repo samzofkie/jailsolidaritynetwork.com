753905e6ce10a4c4a048a2552c7690bbf96e1e10
Sam Zofkie <samzofkie@gmail.com>
2024-09-21 09:18:04 -0500

    Wrote generateThumbnail utility function, finally removed multer from package.json, and added call to generateThumbnail to POST /testimonies/:id/files.

--- backend/api/src/utils.js
+++ backend/api/src/utils.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const sharp = require('sharp');
+
 function formatDate(dateString) {
   const date = new Date(dateString);
   if (isNaN(date.getSeconds()))
@@ -5,6 +8,33 @@ function formatDate(dateString) {
   return date.toISOString().slice(0,7);
 };
 
+async function generateThumbnail(fileData, fileBuffer) {
+  if (fileData.contentType === 'image/jpeg') {
+    fs.writeFileSync(fileData.thumbnailPath, fileBuffer);
+  
+  } else if (fileData.contentType === 'image/png') {
+    sharp(fileBuffer).toFile(fileData.thumbnailPath);
+  
+  } else if (fileData.contentType === 'application/pdf') {
+    const mupdf = await import('mupdf');
+      
+    const doc = mupdf.Document.openDocument(
+      fileBuffer,
+      'application/pdf'
+    );
+    const page = doc.loadPage(0);
+    const pixmap = page.toPixmap(
+      mupdf.Matrix.identity, 
+      mupdf.ColorSpace.DeviceRGB, 
+      false, 
+      true
+    );
+    const jpg = pixmap.asJPEG(90);
+    fs.writeFileSync(fileData.thumbnailPath, Buffer.from(jpg));
+  }
+}
+
 module.exports = {
   formatDate,
+  generateThumbnail,
 };
