778a44c627c5bf67401296791b5e8cf29eeb6efe
Sam Zofkie <samzofkie@gmail.com>
2024-09-23 18:45:21 -0500

    Split tables.sql into two SQL scripts, one to define the tables and another to populate them with test data.

---
+++ backend/db/00-define-tables.sql
@@ -0,0 +1,64 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) UNIQUE,
+  salt VARCHAR(40),
+  hash VARCHAR(128)
+);
+
+CREATE TABLE categories (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) UNIQUE,
+  shorthand VARCHAR(3) UNIQUE
+);
+
+CREATE TABLE divisions (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(15) UNIQUE
+);
+
+CREATE TABLE testimonies (
+  id SERIAL PRIMARY KEY,
+  date_received DATE,
+  length_of_stay INT,
+  gender VARCHAR(15)
+);
+
+CREATE TABLE testimony_divisions (
+  id SERIAL PRIMARY KEY,
+  testimony_id INT NOT NULL REFERENCES testimonies(id),
+  division_id INT NOT NULL REFERENCES divisions(id)
+);
+
+CREATE TABLE testimony_sentences (
+  id SERIAL PRIMARY KEY,
+  sentence TEXT,
+  testimony_id INT NOT NULL REFERENCES testimonies(id)
+);
+
+CREATE TABLE testimony_sentences_categories (
+  id SERIAL PRIMARY KEY,
+  sentence_id INT NOT NULL REFERENCES testimony_sentences(id),
+  category_id INT NOT NULL REFERENCES categories(id)
+);
+
+CREATE TABLE testimony_files (
+  id SERIAL PRIMARY KEY,
+  testimony_id INT NOT NULL REFERENCES testimonies(id),
+  file_name TEXT
+);
+
+INSERT INTO categories (name, shorthand) VALUES
+  ('Basic Services and Materials', 'BSM'),
+  ('Contact and Family', 'CF'),
+  ('Jail Rules and Grievances', 'JRG'),
+  ('Education and Programming', 'EP'),
+  ('Facilities', 'F'),
+  ('Food and Water', 'FW'),
+  ('Legal System', 'LS'),
+  ('Mental and Physical Health and Care', 'MPH'),
+  ('Staff Treatment', 'ST'),
+  ('Violence', 'V'),
+  ('Administration, Corruption, and Budget', 'ACB');
+
+INSERT INTO divisions (name) VALUES
+  ('2'), ('3'), ('4'), ('6'), ('9'), ('10'), ('11'), ('14'), ('Cermak'), ('Solitary');
